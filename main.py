89a739297c08de44e57a357c41d5915615c11e22
hzml <hongzhe2022@163.com>
2025-02-02 13:51:12 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,254 @@
+# -*- encoding=utf8 -*-
+__author__ = "hongzhe"
+
+import logging
+import sys
+
+from PySide6.QtCore import QThread, Signal, QUrl
+from PySide6.QtGui import QDesktopServices
+from PySide6.QtWidgets import QApplication, QMainWindow
+from airtest.core.api import *
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
+from ui_main_window import Ui_MainWindow
+
+# 配置日志输出到文件
+log_file = "task_log.txt"
+logger = logging.getLogger("airtest")
+logger.setLevel(logging.DEBUG)  # 设置日志级别为 DEBUG
+
+# 创建文件处理器，日志写入到文件
+file_handler = logging.FileHandler(log_file, mode='a', encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+
+# 创建日志格式
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# 添加处理器到日志器
+logger.addHandler(file_handler)
+
+# 任务队列
+task_queue = []
+
+
+def add_task(task):
+    """
+    向任务队列添加任务
+    """
+    task_queue.append(task)
+    logger.info(f"任务已添加到队列: {task.__name__}")
+
+
+def execute_task(task):
+    """
+    执行任务并处理异常
+    """
+    try:
+        logger.info(f"开始执行任务: {task.__name__}")
+        task()  # 执行任务
+        logger.info(f"任务执行成功: {task.__name__}")
+    except Exception as e:
+        logger.error(f"任务执行失败: {task.__name__} - 错误: {e}")
+        rollback_task(task)  # 回滚操作
+        return False
+    return True
+
+
+def rollback_task(task):
+    """
+    任务失败时的回滚操作
+    这里你可以根据任务的类型来定义回滚操作
+    """
+    logger.info(f"任务失败，开始回滚: {task.__name__}")
+    swipe_left()
+
+
+def swipe_left():
+    """
+    向左滑动
+    """
+    tv_job_name = poco("com.hpbr.bosszhipin:id/tv_job_name", type="android.widget.TextView")
+    tv_job_name.swipe([-0.9, -0.1], duration=0.1)
+
+
+def parse_page():
+    """
+    解析工作详情页
+    """
+    try:
+        tv_job_name = poco("com.hpbr.bosszhipin:id/tv_job_name", type="android.widget.TextView")
+        tv_description = poco("com.hpbr.bosszhipin:id/tv_description").attr('text') if poco(
+            "com.hpbr.bosszhipin:id/tv_description").exists() else "暂无描述"
+        btn_chat = poco("com.hpbr.bosszhipin:id/btn_chat")
+    except Exception as e:
+        logger.error(f"控件查找失败: {e}")
+        return None, None
+
+    if btn_chat:
+        job_name = tv_job_name.get_text() if tv_job_name else ''
+        return tv_description, btn_chat, job_name
+
+    return None, None, None
+
+
+def filter_job_title(job_name, job_key):
+    """
+    根据用户提供的筛选规则对职位标题进行筛选
+    返回 True 表示符合筛选条件，False 表示不符合
+    """
+    for key in job_key:
+        if key in job_name:
+            return True
+    return False
+
+
+def task_job(job_key):
+    """
+    任务：解析页面内容并执行聊天操作
+    """
+    content, btn_chat, job_name = parse_page()
+    if content and btn_chat and job_name:
+        logger.info(f"职位名称：: {job_name}")
+        logger.info(f"岗位描述: {content}")
+    if content and job_name:
+        if filter_job_title(job_name, job_key):  # 筛选标题是否符合条件
+            logger.info(f"职位标题符合筛选条件: {job_name}")
+            logger.info(f"解析到内容: {content}")
+            btn_chat.click()  # 打招呼
+            time.sleep(0.5)  # 等待页面加载
+            keyevent("BACK")  # 返回详情页
+            time.sleep(0.1)  # 等待页面加载
+            swipe_left()  # 向左滑动
+            time.sleep(1.5)  # 等待页面加载
+        else:
+            logger.info(f"职位标题不符合筛选条件，跳过: {job_name}")
+            swipe_left()
+    else:
+        logger.error("找不到控件，任务失败")  # 如果没有找到控件，任务失败
+        raise Exception("找不到控件，任务失败")
+
+
+# PySide6界面部分
+class TaskWorker(QThread):
+    """
+    后台任务线程类，用于处理任务队列
+    """
+    task_completed = Signal(str)  # 任务完成信号
+    task_failed = Signal(str)  # 任务失败信号
+    progress_updated = Signal(int)  # 进度更新信号
+
+    def __init__(self, job_key, num_tasks):
+        super().__init__()
+        self.job_key = job_key
+        self.num_tasks = num_tasks
+
+    def run(self):
+        # 执行队列中的任务
+        for i in range(self.num_tasks):
+            add_task(lambda: task_job(self.job_key))
+            if execute_task(task_queue[i]):
+                self.task_completed.emit(f"任务 {i + 1} 执行成功")
+            else:
+                self.task_failed.emit(f"任务 {i + 1} 执行失败")
+            self.progress_updated.emit((i + 1) * 100 // self.num_tasks)
+
+
+class MainWindow(QMainWindow):
+
+
+    def __init__(self):
+        super().__init__()
+
+        # 创建 UI 类的实例
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)  # 设置 UI 布局
+
+        # 连接信号和槽
+        self.ui.add_task_button.clicked.connect(self.add_task)
+        self.ui.start_button.clicked.connect(self.start_tasks)
+        self.ui.open_log_button.clicked.connect(self.open_log_file)
+        self.ui.stop_button.clicked.connect(self.stop_tasks)
+
+        self.ui.add_group.clicked.connect(self.add_group)
+
+
+
+    def add_group(self):
+        """浏览器跳转打开加群链接"""
+        url = QUrl("https://qm.qq.com/q/Xdw4VIAIAo")
+        QDesktopServices.openUrl(url)
+
+    def stop_tasks(self):
+        """
+        停止后台任务
+        """
+        if hasattr(self, 'worker') and self.worker.isRunning():
+            self.worker.terminate()  # 停止任务线程
+            self.ui.log_output.append("任务已停止")
+        else:
+            self.ui.log_output.append("没有正在运行的任务")
+
+    def open_log_file(self):
+        """
+        打开日志文件
+        """
+        # 读取日志文件并在文本框中显示
+        try:
+            with open(log_file, 'r', encoding='utf-8') as file:
+                log_content = file.read()
+            self.ui.log_output.setPlainText(log_content)
+        except Exception as e:
+            self.ui.log_output.append(f"无法打开日志文件: {e}")
+
+
+    def add_task(self):
+        """
+        向队列添加任务
+        """
+        filter_text = self.ui.filter_input.text()
+        job_key = filter_text.split(" ") if filter_text else []
+        num_tasks = self.ui.num_tasks_input.value()
+
+        for _ in range(num_tasks):
+            add_task(lambda: task_job(job_key))
+
+        self.ui.log_output.append(f"已添加 {num_tasks} 个任务，筛选规则: {job_key}")
+
+    def start_tasks(self):
+        """
+        启动后台任务线程
+        """
+        filter_text = self.ui.filter_input.text()
+
+        job_key = filter_text.split(",") if filter_text else []
+        num_tasks = self.ui.num_tasks_input.value()
+
+        self.worker = TaskWorker(job_key, num_tasks)
+        self.worker.task_completed.connect(self.on_task_completed)
+        self.worker.task_failed.connect(self.on_task_failed)
+        self.worker.progress_updated.connect(self.update_progress)
+
+        # 在开始执行任务之前初始化Poco
+        auto_setup(__file__)  # 初始化Poco
+        global poco
+        poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+
+        self.worker.start()
+
+    def on_task_completed(self, message):
+        self.ui.log_output.append(f"任务完成: {message}")
+
+    def on_task_failed(self, message):
+        self.ui.log_output.append(f"任务失败: {message}")
+
+    def update_progress(self, progress):
+        self.ui.progress_bar.setValue(progress)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ ui_main_window.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QLineEdit,
+    QMainWindow, QMenuBar, QProgressBar, QPushButton,
+    QSizePolicy, QSpinBox, QStatusBar, QTextEdit,
+    QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(409, 664)
+        MainWindow.setStyleSheet(u"QPushButton {\n"
+"                background-color: #4CAF50;\n"
+"                color: white;\n"
+"                border: 1px solid #4CAF50;\n"
+"                border-radius: 5px;\n"
+"                padding: 10px 20px;\n"
+"                font-size: 14px;\n"
+"                font-weight: bold;\n"
+"                transition: background-color 0.3s, border-color 0.3s;\n"
+"            }\n"
+"            QPushButton:hover {\n"
+"                background-color: #45a049;\n"
+"                border-color: #45a049;\n"
+"            }\n"
+"            QPushButton:pressed {\n"
+"                background-color: #3e8e41;\n"
+"                border-color: #3e8e41;\n"
+"            }")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.log_output = QTextEdit(self.centralwidget)
+        self.log_output.setObjectName(u"log_output")
+        self.log_output.setReadOnly(True)
+
+        self.gridLayout.addWidget(self.log_output, 0, 0, 1, 1)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.progress_bar = QProgressBar(self.centralwidget)
+        self.progress_bar.setObjectName(u"progress_bar")
+
+        self.verticalLayout_2.addWidget(self.progress_bar)
+
+        self.filter_label = QLabel(self.centralwidget)
+        self.filter_label.setObjectName(u"filter_label")
+
+        self.verticalLayout_2.addWidget(self.filter_label)
+
+        self.filter_input = QLineEdit(self.centralwidget)
+        self.filter_input.setObjectName(u"filter_input")
+        self.filter_input.setMinimumSize(QSize(0, 31))
+
+        self.verticalLayout_2.addWidget(self.filter_input)
+
+        self.num_tasks_label = QLabel(self.centralwidget)
+        self.num_tasks_label.setObjectName(u"num_tasks_label")
+
+        self.verticalLayout_2.addWidget(self.num_tasks_label)
+
+        self.num_tasks_input = QSpinBox(self.centralwidget)
+        self.num_tasks_input.setObjectName(u"num_tasks_input")
+        self.num_tasks_input.setMinimumSize(QSize(0, 31))
+        self.num_tasks_input.setMinimum(1)
+        self.num_tasks_input.setMaximum(1000)
+        self.num_tasks_input.setValue(100)
+
+        self.verticalLayout_2.addWidget(self.num_tasks_input)
+
+
+        self.gridLayout.addLayout(self.verticalLayout_2, 1, 0, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.add_task_button = QPushButton(self.centralwidget)
+        self.add_task_button.setObjectName(u"add_task_button")
+        self.add_task_button.setMinimumSize(QSize(0, 40))
+
+        self.verticalLayout.addWidget(self.add_task_button)
+
+        self.start_button = QPushButton(self.centralwidget)
+        self.start_button.setObjectName(u"start_button")
+        self.start_button.setMinimumSize(QSize(0, 40))
+
+        self.verticalLayout.addWidget(self.start_button)
+
+        self.open_log_button = QPushButton(self.centralwidget)
+        self.open_log_button.setObjectName(u"open_log_button")
+        self.open_log_button.setMinimumSize(QSize(0, 40))
+
+        self.verticalLayout.addWidget(self.open_log_button)
+
+        self.stop_button = QPushButton(self.centralwidget)
+        self.stop_button.setObjectName(u"stop_button")
+        self.stop_button.setMinimumSize(QSize(0, 40))
+
+        self.verticalLayout.addWidget(self.stop_button)
+
+        self.add_group = QPushButton(self.centralwidget)
+        self.add_group.setObjectName(u"add_group")
+        self.add_group.setMinimumSize(QSize(0, 40))
+
+        self.verticalLayout.addWidget(self.add_group)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 2, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 409, 33))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u7b80\u5386\u6295\u9012(\u5f00\u6e90\u514d\u8d39\u4ec5\u7528\u4e8e\u5b66\u4e60\u4ea4\u6d41)", None))
+        self.filter_label.setText(QCoreApplication.translate("MainWindow", u"\u8bf7\u8f93\u5165\u7b5b\u9009\u89c4\u5219\uff08\u4ee5\u7a7a\u683c\u5206\u9694\uff09:", None))
+        self.num_tasks_label.setText(QCoreApplication.translate("MainWindow", u"\u8bf7\u8f93\u5165\u4efb\u52a1\u6570\u91cf:", None))
+        self.add_task_button.setText(QCoreApplication.translate("MainWindow", u"\u6dfb\u52a0\u4efb\u52a1", None))
+        self.start_button.setText(QCoreApplication.translate("MainWindow", u"\u5f00\u59cb\u4efb\u52a1", None))
+        self.open_log_button.setText(QCoreApplication.translate("MainWindow", u"\u67e5\u770b\u65e5\u5fd7\u6587\u4ef6", None))
+        self.stop_button.setText(QCoreApplication.translate("MainWindow", u"\u6682\u505c\u4efb\u52a1", None))
+        self.add_group.setText(QCoreApplication.translate("MainWindow", u"\u52a0\u7fa4\u4ea4\u6d41", None))
+    # retranslateUi
+
